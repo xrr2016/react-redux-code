16e43e3456466eff161052ff8e956fbe6b4a23ac
xrr2016 <13286904669@163.com>
2017-03-20 23:40:04 +0800

    :zap: :fire: add react-icons

--- activity_counter/components/ski_day_count.js
+++ activity_counter/components/ski_day_count.js
@@ -1,5 +1,8 @@
 // import {Component} from 'react'
 import '../style/ui.scss'
+import Terrain from 'react-icons/lib/md/terrain'
+import SnowFlake from 'react-icons/lib/ti/weather-snow'
+import Calendar from 'react-icons/lib/fa/calendar'
 
 function percentToDecimal (decimal) {
   return ((decimal * 100) + '%')
@@ -12,13 +15,13 @@ export const SkiDayCount = (props) => {
   return (
     <div className='ski-day-count'>
       <div className='total-days'>
-        <span>{props.total} days</span>
+        <span>{props.total} <Calendar /> days</span>
       </div>
       <div className='powder-days'>
-        <span>{props.powder} days</span>
+        <span>{props.powder} <SnowFlake /> days</span>
       </div>
       <div className='backcountry-days'>
-        <span>{props.backcountry} hiking day</span>
+        <span>{props.backcountry} <Terrain /> hiking day</span>
       </div>
       <div className='goal'>
         <span>{calcGoalProgress(props.total, props.goal)}</span>

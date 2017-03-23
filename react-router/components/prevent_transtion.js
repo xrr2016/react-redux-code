4ac6590efff6ce15d370e2ff70a713dabda9a2c5
xrr2016 <13286904669@163.com>
2017-03-23 23:25:18 +0800

    :bulb: add

---
+++ react-router/components/prevent_transtion.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+const PreExample = () => {
+  <Router>
+    <div></div>
+  </Router>
+}
+
+export default PreExample

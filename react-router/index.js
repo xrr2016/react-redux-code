1495988a2f7460f2f4002e34f79ad743d88c9450
xrr2016 <13286904669@163.com>
2017-03-23 23:21:13 +0800

    :zap: custom_link

--- react-router/index.js
+++ react-router/index.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { render } from 'react-dom'
 // import BasicExample from './components/basic_example'
 // import ParamsExample from './components/params_example'
-import AuthExample from './components/auth_example'
+// import AuthExample from './components/auth_example'
+import CustomLinkExample from './components/custom_link'
 
 render(
-  <AuthExample username={'xrr'} />,
+  <CustomLinkExample />,
   document.getElementById('root')
 )
